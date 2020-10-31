0167779562c42f0bb978ce20acf9a1e4b277a15a
Robin Schroer <sulami@peerwire.org>
2020-10-31 17:14:46 +0100

    Scaffold a new project

---
+++ circleci-api.el
@@ -0,0 +1,35 @@
+;;; circleci-api.el --- Bindings for the CircleCI API. -*- lexical-binding: t -*-
+
+;; Author: Robin Schroer
+;; Maintainer: Robin Schroer
+;; Version: 0.1
+;; Homepage: https://github.com/sulami/circleci-api
+;; Package-Requires: ((emacs "25.1"))
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Bindings for the CircleCI API.
+
+;;; Code:
+
+
+(provide 'circleci-api)
+
+;;; circleci-api.el ends here
